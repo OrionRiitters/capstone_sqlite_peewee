9e0c972d3a5c40d2453852245707dca2213d4140
Orion Riitters <orionriitters@gmail.com>
2019-02-14 17:05:45 -0600

    Added UI.add_row() and test.py to run it.

---
+++ part_1/test.py
@@ -0,0 +1,6 @@
+from ui import UI
+
+new_ui = UI()
+
+yes, no, maybe = new_ui.add_row()
+print(yes + no + maybe)
--- part_1/ui.py
+++ part_1/ui.py
@@ -2,8 +2,14 @@ import db_manager
 
 class UI():
 
-    def add_row():
-        pass
+    def add_row(self):
+        row_name = input('Please enter the name for the record you are entering:\n')
+        row_country = input('Please enter the country for the record you are entering:\n')
+        row_catches = input('Please enter the number of catches for the record you are entering:\n')
+        return row_name, row_country, row_catches
+
+
+
 
 
     def search_by_name():
