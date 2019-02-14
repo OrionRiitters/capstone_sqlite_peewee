986f617a389df8efa0e351310be86b524ffc37df
Orion Riitters <orionriitters@gmail.com>
2019-02-14 16:32:38 -0600

    Created class skeletons in part 1.

---
+++ part_1/db_manager.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+class ManageDB():
+
+    def create():
+        pass
+
+
+    def read():
+        pass
+
+
+    def update():
+        pass
+
+
+    def delete():
+        pass
+
---
+++ part_1/ui.py
@@ -0,0 +1,18 @@
+import db_manager
+
+class UI():
+
+    def add_row():
+        pass
+
+
+    def search_by_name():
+        pass
+
+
+    def update_catches():
+        pass
+
+
+    def delete_by_name():
+        pass
