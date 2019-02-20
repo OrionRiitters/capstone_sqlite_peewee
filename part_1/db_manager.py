e27d4480f11008351020deac63f7ae3b3a419741
Orion Riitters <orionriitters@gmail.com>
2019-02-19 22:14:02 -0600

    Finished part_1.

--- part_1/db_manager.py
+++ part_1/db_manager.py
@@ -2,7 +2,11 @@ import sqlite3
 
 class ManageDB():
     """
-    This class accesses and modifies an sqlite database. Implements the singleton class pattern from https://www.tutorialspoint.com/python_design_patterns/python_design_patterns_singleton.htm
+    This class accesses and modifies an sqlite database. Implements the singleton class pattern
+    from https://www.tutorialspoint.com/python_design_patterns/python_design_patterns_singleton.htm
+    This singleton pattern is  unnecessary but it works and I don't have time to un-implement it.
+    These methods are all pretty self-explanatory so I will not be writing comments for each of
+    them.
     """
 
     __instance = None
@@ -25,7 +29,7 @@ class ManageDB():
                 db = sqlite3.connect('juggling_records')
                 cur = db.cursor()
                 with db:
-                    cur.execute('Create table if not exists records (name text, country text, catches int)')
+                    cur.execute('CREATE TABLE IF NOT EXISTS records (name text, country text, catches int)')
             except sqlite3.Error as e:
                 print('Error creating database..')
                 print(e)
@@ -33,26 +37,55 @@ class ManageDB():
                 db.close()
 
     def add_row(self, row_name, row_country, row_catches):
+
         try:
             db = sqlite3.connect('juggling_records')
             cur = db.cursor()
             with db:
-                cur.execute('insert into records values (?, ?, ?)', (row_name, row_country, row_catches))
-            print('Successfully inserted row!')
+                cur.execute('INSERT INTO records VALUES (?, ?, ?)', (row_name, row_country, row_catches))
         except sqlite3.Error as e:
             print('Error inserting row (it was not updated).')
-            print(e)
         finally:
             db.close()
 
-    def read():
-        pass
+
+    def read(self, input):
+
+        upper_input = input.upper()
+        row_list = []
+
+        try:
+            db = sqlite3.connect('juggling_records')
+            cur = db.cursor()
+            with db:
+                for row in cur.execute('SELECT * FROM records WHERE UPPER(name) like ?', [upper_input]):
+                    row_list.append(row)
+
+            return row_list
+
+        except sqlite3.Error as e:
+            print('Error: Could not read rows.')
+            print(e)
+
+        finally:
+            db.close()
 
 
-    def update():
-        pass
+    def delete(self, input):
+
+        upper_input = input.upper()
+
+        try:
+            db = sqlite3.connect('juggling_records')
+            cur = db.cursor()
+            with db:
+                cur.execute('DELETE FROM records WHERE UPPER(name) like ?', [upper_input])
 
 
-    def delete():
-        pass
+        except sqlite3.Error as e:
+            print('Error: Could not delete row.')
+            print(e)
+
+        finally:
+            db.close()
 
--- part_1/ui.py
+++ part_1/ui.py
@@ -4,29 +4,105 @@ class UI():
 
     manageDB = ManageDB.getInstance()
 
-    def add_row(self):
-        
 
+    def search_by_name(self):
+        """
+        This method takes user input and calls manageDB.read with the input as
+        an argument
+        """
+
+        user_input = input('What is the name of the record holder you seek?\n')
+        print('\n')
+        rows_returned = self.manageDB.read(user_input)
+
+        if rows_returned:
+            print('\n%-20s%-20s%-20s' % ('Name', 'Country', 'Number of Catches'))
+            [print('%-20s%-20s%-20i' % row) for row in rows_returned]
+        else:
+            print('\nNo rows match your search criteria!\n')
+
+
+    def add_row(self):
+        """
+        This method takes user input and calls manageDB.add_row with the input
+        as the method's arguments
+        """
         row_name = input('Please enter the name for the record you are entering:\n')
         row_country = input('Please enter the country for the record you are entering:\n')
         row_catches = 0
         try:
             row_catches += int(input('Please enter the number of catches for the record you are entering:\n'))
+
         except ValueError:
-            print('Error: Please be sure to enter an integer for the number of catches. Row not updated.')
+            print('Error: Please be sure to enter an integer for the number of catches. Row not updated.\n')
             return
 
-        self.manageDB.add_row(row_name, row_country, row_catches)
-    
+
+        if not self.manageDB.read(row_name):
+            print('\n')
+            self.manageDB.add_row(row_name, row_country, row_catches)
+            print('Successfully inserted row!\n')
+        else:
+            print('\nOops! That name is already in the database. Try removing it or updating the number of catches.\n')
 
 
-    def search_by_name():
-        pass
+    def update_catches(self):
+        """
+        This method takes user input and calls manageDB.read, manageDB.add_row
+        and manageDB.delete with the input as the method's arguments.
+        """
+
+        name_input = input('Enter record holder name:\n')
+        try:
+            catches_input = int(input('Enter number of catches:\n'))
+        except ValueError:
+            print('Error: Please be sure to enter an integer for the number of catches. Row not updated.\n')
+            return
+
+        row_returned = self.manageDB.read(name_input)
+        print('\n')
+        if row_returned:
+            self.manageDB.add_row(row_returned[0][0], row_returned[0][1], catches_input)
+            self.manageDB.delete(name_input)
+            print('Successfully updated row!\n')
+        else:
+            print('\nNo rows match your search criteria!')
 
 
-    def update_catches():
-        pass
+    def delete_by_name(self):
+        """
+        This method takes user input and calls manageDB.read and manageDB.delete
+        with the input as the method's arguments.
+        """
+
+        name_input = input('Enter the name of the record holder you would like to delete:\n')
+
+        if self.manageDB.read(name_input):
+            self.manageDB.delete(name_input)
+            print('Successfully deleted row!')
+        else:
+            print('\nNo rows match your search criteria!')
+
+    def exit_prog(self):
+        exit()
+
+    def incorrect_input(self):
+        print('Please type a correct input option!\n')
 
 
-    def delete_by_name():
-        pass
+    def menu(self):
+        """
+        This method simply displays the user's menu command options.
+        """
+        print('\nTake a look at these nice options!')
+        print('1 - Search database by record holder name')
+        print('2 - Add row to database')
+        print('3 - Update number of catches for existing record')
+        print('4 - Delete record by record holder name')
+        print('0 - Exit program\n\n')
+
+        user_input = input('')
+        print('\n')
+
+        return user_input
+
